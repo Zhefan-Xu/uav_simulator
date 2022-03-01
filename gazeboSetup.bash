178b0b771780ed566a2c247a43668f05096016f2
zhefan <zhefanx@andrew.cmu.edu>
2022-02-28 20:13:43 -0500

    build

---
+++ gazeboSetup.bash
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${dir}/models
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:${dir}/plugins
