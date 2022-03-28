0962575b8a820f097517160c25772f38d5e99fe0
zhefan <zhefanx@andrew.cmu.edu>
2022-03-27 23:16:10 -0400

    udpate

--- gazeboSetup.bash
+++ gazeboSetup.bash
@@ -2,4 +2,5 @@
 
 dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${dir}/models
+export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:${dir}/urdf
 export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:${dir}/plugins
